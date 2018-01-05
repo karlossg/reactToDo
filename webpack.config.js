3091b6d97abf7eb2b62404541138fa44c59fbe65
Karol <karol.gratus@gmail.com>
2018-01-05 15:45:37 +0100

    i hate you webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,53 +3,53 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeJsPlugin = require('optimize-js-plugin');
 
-var env = process.env.NODE_ENV || 'development';
+const NODE_MODULES = path.resolve(__dirname, 'node_modules');
 
-var plugins = [
-  new HtmlWebpackPlugin({
-    template: 'src/index.html',
-    filename: 'index.html',
-    inject: 'body'
-  })
-];
-
-console.log('NODE_ENV:', env);
-
-if (env === 'production') {
-  plugins.push(
-    new UglifyJsPlugin(),
-    new OptimizeJsPlugin({
-      sourceMap: false
+module.exports = env => {
+  var plugins = [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: 'index.html',
+      inject: 'body'
     })
-  );
-}
+  ];
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'app.bundle.js',
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        use: [
-          { loader: 'style-loader' },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true
+  if (process.env.NODE_ENV === 'production') {
+    plugins.push(
+      new UglifyJsPlugin(),
+      new OptimizeJsPlugin({
+        sourceMap: false
+      })
+    );
+  }
+
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'app.bundle.js',
+      publicPath: '/'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.css$/,
+          use: [
+            { loader: 'style-loader' },
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true
+              }
             }
-          }
-        ]
-      }
-    ]
-  },
-  plugins: [plugins]
+          ]
+        }
+      ]
+    },
+    plugins: plugins
+  };
 };
